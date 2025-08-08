353ffb1699e14f6ccd618be1882b9785ffc3d20f
Harshit Jain <harshitjain0502@gmail.com>
2025-08-09 03:57:54 +0530

    Week 1 Day 3

--- mini-lsm-starter/src/block.rs
+++ mini-lsm-starter/src/block.rs
@@ -12,14 +12,12 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#![allow(unused_variables)] // TODO(you): remove this lint after implementing this mod
-#![allow(dead_code)] // TODO(you): remove this lint after implementing this mod
-
 mod builder;
 mod iterator;
 
+use crate::key::KeyVec;
 pub use builder::BlockBuilder;
-use bytes::Bytes;
+use bytes::{Buf, BufMut, Bytes};
 pub use iterator::BlockIterator;
 
 /// A block is the smallest unit of read and caching in LSM tree. It is a collection of sorted key-value pairs.
@@ -31,12 +29,53 @@ pub struct Block {
 impl Block {
     /// Encode the internal data to the data layout illustrated in the course
     /// Note: You may want to recheck if any of the expected field is missing from your output
+    /*
+    ----------------------------------------------------------------------------------------------------
+    |             Data Section             |              Offset Section             |      Extra      |
+    ----------------------------------------------------------------------------------------------------
+    | Entry #1 | Entry #2 | ... | Entry #N | Offset #1 | Offset #2 | ... | Offset #N | num_of_elements |
+    ----------------------------------------------------------------------------------------------------
+
+    -----------------------------------------------------------------------
+    |                           Entry #1                            | ... |
+    -----------------------------------------------------------------------
+    | key_len (2B) | key (keylen) | value_len (2B) | value (varlen) | ... |
+    -----------------------------------------------------------------------
+
+    -------------------------------
+    |offset|offset|num_of_elements|
+    -------------------------------
+    |   0  |  12  |       2       |
+    -------------------------------
+    */
     pub fn encode(&self) -> Bytes {
-        unimplemented!()
+        let mut encoded_data = self.data.clone();
+        let num_of_elements = self.offsets.len();
+        for offset in &self.offsets {
+            encoded_data.put_u16(*offset);
+        }
+        encoded_data.put_u16(num_of_elements as u16);
+        Bytes::from(encoded_data)
     }
 
     /// Decode from the data layout, transform the input `data` to a single `Block`
     pub fn decode(data: &[u8]) -> Self {
-        unimplemented!()
+        let num_of_elements = (&data[data.len() - 2..]).get_u16() as usize;
+        let data_end = data.len() - 2 - num_of_elements * 2;
+
+        Self {
+            data: data[..data_end].to_vec(),
+            offsets: data[data_end..data.len() - 2]
+                .chunks_exact(2)
+                .map(|chunk| u16::from_le_bytes([chunk[1], chunk[0]]))
+                .collect::<Vec<u16>>(),
+        }
+    }
+
+    pub fn get_first_key(&self) -> KeyVec {
+        let mut buf = &self.data[..];
+        let key_len = buf.get_u16();
+        let key = &buf[..key_len as usize];
+        KeyVec::from_vec(key.to_vec())
     }
 }
--- mini-lsm-starter/src/block/builder.rs
+++ mini-lsm-starter/src/block/builder.rs
@@ -12,8 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#![allow(unused_variables)] // TODO(you): remove this lint after implementing this mod
-#![allow(dead_code)] // TODO(you): remove this lint after implementing this mod
+use bytes::BufMut;
 
 use crate::key::{KeySlice, KeyVec};
 
@@ -34,23 +33,45 @@ pub struct BlockBuilder {
 impl BlockBuilder {
     /// Creates a new block builder.
     pub fn new(block_size: usize) -> Self {
-        unimplemented!()
+        Self {
+            offsets: Vec::new(),
+            data: Vec::new(),
+            block_size,
+            first_key: KeyVec::new(),
+        }
     }
 
     /// Adds a key-value pair to the block. Returns false when the block is full.
     /// You may find the `bytes::BufMut` trait useful for manipulating binary data.
     #[must_use]
     pub fn add(&mut self, key: KeySlice, value: &[u8]) -> bool {
-        unimplemented!()
+        if self.is_empty() {
+            self.first_key = key.to_key_vec();
+        } else {
+            let curr_block_size = self.data.len() + self.offsets.len() * 2 + 2; // 2 bytes for each offset and 2 bytes for num_of_elements
+            if curr_block_size + key.len() + value.len() + 3 * 2 > self.block_size {
+                return false;
+            }
+        }
+
+        self.offsets.push(self.data.len() as u16); // Store the offset of the current key-value pair
+        self.data.put_u16(key.len() as u16); // Key length
+        self.data.put(key.raw_ref()); // Key data
+        self.data.put_u16(value.len() as u16); // Value length
+        self.data.put(value); // Value data
+        true
     }
 
     /// Check if there is no key-value pair in the block.
     pub fn is_empty(&self) -> bool {
-        unimplemented!()
+        self.offsets.is_empty()
     }
 
     /// Finalize the block.
     pub fn build(self) -> Block {
-        unimplemented!()
+        Block {
+            data: self.data,
+            offsets: self.offsets,
+        }
     }
 }
